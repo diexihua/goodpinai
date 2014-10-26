3c948c6ad5497ed7c8d4149bd9e8862da39e169a
chunyang <sunyang121027.me@gmail.com>
2014-10-26 16:41:53 +0800

    修改于2014-10-26 16:41

--- goodpinai/Public/Common/Js/nav.js
+++ goodpinai/Public/Common/Js/nav.js
@@ -8,9 +8,26 @@
 			}else{
 				navS();
 			}
-			stopBubble(this);
 		}
-		oNavMain.onclick=function(){
+
+		function navS(){
+			if(navNum==false){
+				nav.style.left='0px';
+				navNum=true;
+			}else{
+				nav.style.left='-60px';
+				navNum=false;
+			}
+		}
+	    // function stopBubble(e){
+	    // 	var e=e||windw.event;  
+	    //     if(e&&e.stopPropagation){  
+	    //         e.stopPropagation();  
+	    //     }else{   
+	    //         e.cancelBubble = true;  
+	    //     }  
+    	// }
+		oNavMain.onclick=function(e){
 			if(navNum==true){
 				var arr=oNav.getElementsByTagName('li');
 				for(var i=0;i<arr.length;i++){
@@ -18,32 +35,18 @@
 				}
 			}
 			navS();
-			stopBubble(this);
-		};
-		function navS(){
-			if(navNum==false){
-				nav.style.left=0;
-				navNum=true;
-			}else{
-				nav.style.left='-60px';
-				navNum=false;
-			}
-		}
-	    function stopBubble(e){  
-	        if(e&&e.stopPropagation){  
-	            e.stopPropagation();  
-	        }else{   
-	            window.event.cancelBubble = true;  
-	        }  
-    	}  
+			var e=e||event; 
+			e.cancelBubble = true; 
+		};  
 		oNav.onmouseover=function(){
 			var arr=oNav.getElementsByTagName('li');
 			for(var i=0;i<arr.length;i++){
 				arr[i].style.width='150px';
 			}
 		}
-		oNav.onclick=function(){
-			stopBubble(this);
+		oNav.onclick=function(e){
+			var e=e||windw.event; 
+			e.cancelBubble = true; 
 		}
 		document.onclick=function(){
 			var arr=oNav.getElementsByTagName('li');
